a85e2673fb35fea15f9c614f328c7b206991d0a4
Helia_Karisani <helia.karisani@gmail.com>
2025-08-02 15:23:14 -0400

    Create Operation

--- app.py
+++ app.py
@@ -18,6 +18,22 @@ def get_transactions():
     return render_template('transactions.html', transactions=transactions)
 
 # Create operation
+#GET for displaying the form to the user and POST for processing the form data sent by the user
+@app.route("/add", methods=['GET', 'POST'])
+def add_transaction():
+    if request.method == 'POST':
+        # Create a new transaction object using form field values
+        new_transaction = {
+            #Generate a new ID based on the current length of the transactions list
+            'id': len(transactions) + 1,
+            'date': request.form['date'],
+            #Get the 'amount' field value from the form and convert it to a float
+            'amount': float(request.form['amount'])
+        }
+        transactions.append(new_transaction)
+        return redirect(url_for('get_transactions'))
+    return render_template('add_transaction.html')
+
 
 # Update operation
 
